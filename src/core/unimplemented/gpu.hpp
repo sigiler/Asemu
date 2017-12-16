e96dcb64d220df390557de72732cfd93d66da349
sigiler <gildfpsilva@gmail.com>
2017-12-16 12:11:39 +0000

    Revert "more unused stuff"
    This reverts commit 85f76b3944fee18981ce2e6730c36a813b79d083.

---
+++ src/core/unimplemented/gpu.hpp
@@ -0,0 +1,20 @@
+
+#pragma once
+
+struct gpu2d {
+	// sprites
+	// tilemapper
+	// backgrounds
+};
+
+struct gpu3d {
+	// matrices
+};
+
+struct renderer_soft {
+
+};
+
+struct renderer_hard {
+
+};
---
+++ src/core/unimplemented/link.hpp
@@ -0,0 +1,17 @@
+
+#pragma once
+
+// serial, usb
+
+
+struct cable {
+	//bool transfering;
+	//u8* incoming_data;
+	//u8* outgoing_data;
+	//u32 incoming_index;
+	//u32 outgoing_index;
+};
+
+struct wifi {
+
+};
